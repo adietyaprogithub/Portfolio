80d732b1ca63f5fe5de73647dae92fd9fbf8b875
adietyaprogithub <adityashende9922@gmail.com>
2023-07-13 23:38:18 +0530

    newer

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,20 +1,19 @@
-import React from 'react'
-import Skills from '../Tools & Skills/ToolsLibraries'
-import Contact from '../Contact/Contact'
-import Navbar from '../NavbarPage/Navbar'
-import MainNavbar from '../NavbarPage/MainNavbar'
-import Work from '../Work/Work'
-import { BrowserRouter , Routes , Route } from 'react-router-dom'
+import React from "react";
+import Skills from "../Tools & Skills/ToolsLibraries";
+import Contact from "../Contact/Contact";
+import Navbar from "../NavbarPage/Navbar";
+import MainNavbar from "../NavbarPage/MainNavbar";
+import Work from "../Work/Work";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function Main() {
   return (
     <div>
-        <Navbar/>
-        <MainNavbar/>
-        <Work/>
-        <Skills/>
-        <Contact/>
-        
+      <Navbar />
+      <MainNavbar />
+      <Work />
+      <Skills />
+      <Contact />
     </div>
-  )
+  );
 }
